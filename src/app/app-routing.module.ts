a532684b6f22753253a0bc1631b5ec5a5ccb101a
Daniel Batista <dani_batista20@hotmail.com>
2021-04-16 15:26:48 +0100

    3# Course Project - The Basics && add: bootstrap4 scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: "",
+  // },
+  // {
+  //   path: "home",
+  // },
+  // {
+  //   path: "about",
+  // },
+  // {
+  //   path: "contact",
+  // },
+  // {
+  //   path: "user", // lazy
+  // },
+  // {
+  //   path: "recepies", // lazy
+  // },
+  // {
+  //   path: "shopping", // lazy
+  // },
+  // {
+  //   path: "404",
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
