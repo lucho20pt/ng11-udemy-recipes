64a1ef130985fecf67b03fbd2aa5c468938bde2a
Daniel Batista <dani_batista20@hotmail.com>
2021-04-20 14:55:16 +0100

    3# Course Project - The Basics | add: recipe interface, recipe-list content output with ngFor

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    {
+      id: 1,
+      name: 'Hamburger',
+      description: "Hamburger com ovo e salada",
+      imagePath: "//images.freeimages.com/images/large-previews/9ac/fastfood-01-1329727.jpg"
+    },
+    {
+      id: 2,
+      name: 'Salada',
+      description: "Salada de alface e tomate",
+      imagePath: "https://images.freeimages.com/images/large-previews/f3f/food-1171568.jpg"
+    },
+  ];
 
   constructor() { }
 
