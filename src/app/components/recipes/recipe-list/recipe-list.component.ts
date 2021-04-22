34c9eb5f5564b61298781679bd30d3a045b27bb0
Daniel Batista <dani_batista20@hotmail.com>
2021-04-22 12:43:24 +0100

    6# Course Project - Components & Databinding 3# Recipe Data with Property Binding @Input() - parent -> child

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from 'src/app/shared/model/recipe';
 
 @Component({
@@ -8,20 +8,7 @@ import { Recipe } from 'src/app/shared/model/recipe';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [
-    {
-      id: 1,
-      name: 'Hamburger',
-      description: "Hamburger com ovo e salada",
-      imagePath: "//images.freeimages.com/images/large-previews/9ac/fastfood-01-1329727.jpg"
-    },
-    {
-      id: 2,
-      name: 'Salada',
-      description: "Salada de alface e tomate",
-      imagePath: "https://images.freeimages.com/images/large-previews/f3f/food-1171568.jpg"
-    },
-  ];
+  @Input('data-recipes') recipes!: Recipe[];
 
   constructor() { }
 
--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/shared/model/recipe';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    {
+      id: 1,
+      name: 'Hamburger',
+      description: "Hamburger com ovo e salada",
+      imagePath: "//images.freeimages.com/images/large-previews/9ac/fastfood-01-1329727.jpg"
+    },
+    {
+      id: 2,
+      name: 'Salada',
+      description: "Salada de alface e tomate",
+      imagePath: "https://images.freeimages.com/images/large-previews/f3f/food-1171568.jpg"
+    },
+  ];
 
   constructor() { }
 
