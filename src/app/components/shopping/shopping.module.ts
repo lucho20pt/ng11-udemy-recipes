a773abd11aafcaaee062e4aeab934136c4959ebb
Daniel Batista <dani_batista20@hotmail.com>
2021-04-29 19:36:35 +0100

    10# Services & Dependency Inject 5# Adding the Shopping List Service

--- src/app/components/shopping/shopping.module.ts
+++ src/app/components/shopping/shopping.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
+
 import { ShoppingRoutingModule } from './shopping-routing.module';
 import { ShoppingComponent } from './shopping.component';
 import { ShopListComponent } from './shop-list/shop-list.component';
