a7b50d15c6aee22d5b4c931e8de852577fd7a0d2
Daniel Batista <dani_batista20@hotmail.com>
2021-04-29 17:59:32 +0100

    10# Services & Dependency Inject - upd: recipes

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -3,8 +3,32 @@ import { Recipe } from 'src/app/shared/model/recipe';
 
 @Component({
   selector: 'app-recipes',
-  templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss']
+  template: `
+    <hr>
+    <aside class="py-2">
+      <div class="row">
+        <div class="col-12">
+          <button class="btn btn-success">New Recipe</button>
+        </div>
+      </div>
+    </aside>
+    <hr>
+    <div class="row">
+      <!-- recipes-list -->
+      <div class="col-md-6">
+        <app-recipe-list></app-recipe-list>
+      </div>
+      <!-- recipe-detail -->
+      <div class="col-md-5 ml-auto">
+        <app-recipe-detail></app-recipe-detail>
+      </div>
+    </div>
+  `,
+  styles: [`
+    p{
+      color:red;
+    }
+  `]
 })
 export class RecipesComponent implements OnInit {
 
--- src/app/components/recipes/recipes.module.ts
+++ src/app/components/recipes/recipes.module.ts
@@ -10,9 +10,6 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
-
-
-
 @NgModule({
   declarations: [
     RecipesComponent,
