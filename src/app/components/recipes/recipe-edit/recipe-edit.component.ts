57b0b16e630de91812f98902e7f539cc73a3732e
Daniel Batista <dani_batista20@hotmail.com>
2021-05-12 02:13:32 +0100

    12# Routing 13# Programmatic Navigation check editMode

--- src/app/components/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/components/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe } from 'src/app/shared/model/recipe';
+import { RecipeService } from 'src/app/shared/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -11,7 +13,10 @@ import { ActivatedRoute, Params } from '@angular/router';
       <div class="row">
 
         <div class="col-8">
-          hello
+          <!-- <h2>{{recipe.name}}</h2>
+          <p>{{recipe.description}}</p>
+          <p>{{recipe.imagePath}}</p>
+          <p>{{recipe.ingredients}}</p> -->
         </div>
 
       </div>
@@ -23,22 +28,26 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class RecipeEditComponent implements OnInit {
 
-  index?: number;
-  isNew?: boolean = false;
+  index!: number;
+  editMode?: boolean = false;
+  recipe!: Recipe
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
   ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params : Params) => {
-        this.isNew = params['index'] ? false : true;
-        console.log(this.isNew);
-        if( this.isNew ) {
-          console.log('"new" mode');
-        }else{
+        this.editMode = params['index'] != null;
+        this.index = params['index'];
+        console.log(this.editMode);
+        // this.recipe = this.recipeService.getRecipeByIndex(this.index);
+        if( this.editMode ) {
           console.log('"edit" mode');
+        }else{
+          console.log('"new" mode');
         }
       }
     )
