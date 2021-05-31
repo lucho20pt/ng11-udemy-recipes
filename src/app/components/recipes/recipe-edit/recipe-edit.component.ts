8cf5c5e583b2dd560a445a7c09d9fcbf02483dbb
Daniel Batista <dani_batista20@hotmail.com>
2021-05-31 18:13:46 +0100

    16# Forms 11# Creating the Form For Editing Recipes

--- src/app/components/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/components/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Ingredient } from 'src/app/shared/model/ingredient';
 import { Recipe } from 'src/app/shared/model/recipe';
 import { RecipeService } from 'src/app/shared/services/recipe.service';
 
@@ -14,7 +16,8 @@ export class RecipeEditComponent implements OnInit {
 
   index!: number;
   editMode?: boolean = false;
-  recipe!: Recipe
+  recipe!: Recipe;
+  recipeForm!: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,17 +27,36 @@ export class RecipeEditComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe(
       (params : Params) => {
-        this.editMode = params['index'] != null;
         this.index = params['index'];
-        console.log(this.editMode);
+        this.editMode = params['index'] != null;
+        console.log('isEditMode = ', this.editMode);
         this.recipe = this.recipeService.getRecipeByIndex(this.index);
-        if( this.editMode ) {
-          console.log('"edit" mode');
-        }else{
-          console.log('"new" mode');
-        }
+
+        this.initRecipeForm();
       }
     )
+
+  }
+
+  initRecipeForm(): void {
+    let name = '';
+    let imagePath = '';
+    let description = '';
+
+    if( this.editMode ) {
+      const recipe = this.recipeService.getRecipeByIndex(this.index);
+      // console.log(recipe);
+      name = recipe.name;
+      imagePath = recipe.imagePath;
+      description = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(name, Validators.required),
+      'imagePath': new FormControl(imagePath, Validators.required),
+      'description': new FormControl(description, Validators.required),
+    });
+
   }
 
 }
--- src/app/components/recipes/recipes.module.ts
+++ src/app/components/recipes/recipes.module.ts
@@ -9,6 +9,7 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
   imports: [
     CommonModule,
     RecipesRoutingModule,
-    DirectivesModule
+    DirectivesModule,
+    ReactiveFormsModule
   ],
   providers: []
 })
